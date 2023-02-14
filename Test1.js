786f909275b8cfb1b1ff0b22caa5b77e66307a87
oyetade <you@example.com>
2023-02-14 04:51:57 +0100

    first Commit

---
+++ Test1.js
@@ -0,0 +1,30 @@
+let CurrentYear = 2023
+let LastYear = CurrentYear - 1
+let NextYear = CurrentYear + 1
+undefined
+console.log(CurrentYear)
+console.log(LastYear)
+console.log(NextYear)
+VM622:1 2023
+VM622:2 2022
+VM622:3 2024
+undefined
+let Number1 = 5
+let Number2 = 3
+
+let Addition = Number1+Number2
+let Subtraction = Number1-Number2
+let multiplication = Number1*Number2
+let Division = Number1/Number2
+let Remainder = Number1 % Number2
+
+console.log(Addition)
+console.log(Subtraction)
+console.log(multiplication)
+console.log(Division)
+console.log(Remainder)
+VM1217:10 8
+VM1217:11 2
+VM1217:12 15
+VM1217:13 1.6666666666666667
+VM1217:14 2
